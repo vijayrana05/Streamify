b32efcabe340561e8f832bf8b39477066e04dcc0
Vijay Singh <vijayrana98120@gmail.com>
2025-10-25 08:48:01 +0530

    added chat page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -40,7 +40,19 @@ const App = () => {
             )
           }
         />
-        <Route path="/chat" element={isAuthenticated ? <ChatPage /> : <Navigate to="/login" />} />
+        <Route
+          path="/chat/:id"
+          element={
+            isAuthenticated && isOnboarded ? (
+              <Layout showSidebar={false}>
+                <ChatPage />
+              </Layout>
+            ) : (
+              <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
+            )
+          }
+        />
+        {/* <Route path="/chat" element={isAuthenticated ? <ChatPage /> : <Navigate to="/login" />} /> */}
         <Route path="/call" element={isAuthenticated ? <CallPage /> : <Navigate to="/login" />} />
         <Route path="/onboarding" element={ isAuthenticated ? ( !isOnboarded ? ( <OnboardingPage /> ) : ( <Navigate to="/" /> ) ) : ( <Navigate to="/login" /> ) } />
 
